1ee78af4a529abe0f7c8e7d88b6e988fbe26b7f3
Saim Zahid <saimzahid@me.com>
2016-09-29 16:31:08 -0400

    Commented out uglier in config

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,29 @@ ActiveRecord::Schema.define(version: 20160923193841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  end
+
   create_table "devices", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "group_name"
     t.integer  "group_id"
     t.boolean  "assigned"
   end
@@ -48,6 +66,16 @@ ActiveRecord::Schema.define(version: 20160923193841) do
     t.boolean  "active",     default: false
   end
 
+  create_table "qr_codes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transferof_ownerships", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -61,6 +89,7 @@ ActiveRecord::Schema.define(version: 20160923193841) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.integer  "group_id"
     t.boolean  "admin",                  default: false
     t.string   "name"
     t.string   "image"
