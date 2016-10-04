25beb355eafa214b7a6db376a96ca19a94670607
Saim Zahid <saimzahid@me.com>
2016-10-04 17:15:01 -0400

    Minor changes to relation between user and groups.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    respond_with User.all.as_json(only: [:id, :name, :email, :admin])
+    respond_with User.all.as_json(only: [:id, :name, :email, :admin], methods:[:device_names])
   end
 end
