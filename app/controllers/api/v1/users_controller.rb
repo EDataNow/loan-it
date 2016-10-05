8f3eb332b61b7c727382f06e7a00bde56d76b0cc
Saim Zahid <saimzahid@me.com>
2016-10-05 12:14:45 -0400

    Renamed Jbuilder file to avoid conflicts with original index action. Created new route to match new #usersindex action with the Jbuilder file

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,9 @@ class Api::V1::UsersController < ApplicationController
   def index
     respond_with User.all.as_json(only: [:id, :name, :email, :admin, :group_id], methods:[:device_names])
   end
+
+  def usersindex
+  	@users = User.all
+    @groups = Group.all
+  end
 end
